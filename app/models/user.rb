1c50f3cf2ac76caa27a151a64b38c7472be8e62a
Ezequiel Salas <salaseze@gmail.com>
2023-08-14 20:27:05 -0300

    strong parameters for sign_up and sign_in and login via username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,26 @@ class User < ApplicationRecord
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtBlacklist
 
+  attr_writer :login
+
+  def login
+    @login || username || email
+  end
+
+  # Overrides the devise method find_for_authentication
+  # Allow users to Sign In using their username or email address
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(['lower(username) = :value OR lower(email) = :value', { :value => login.downcase }]).first
+    elsif conditions.key?(:username) || conditions.key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
   ##############################################################################
   # VALIDATIONS
   ##############################################################################
-  validates :email, :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
   validates :full_name, presence: true
 end
