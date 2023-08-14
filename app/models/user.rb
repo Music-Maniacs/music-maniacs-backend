6808386c357184e7f775a6365c03b49bd638320c
Ezequiel Salas <salaseze@gmail.com>
2023-08-13 21:28:28 -0300

    bye confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable,
          :validatable,
          :registerable,
-         :confirmable,
          :recoverable,
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtBlacklist
