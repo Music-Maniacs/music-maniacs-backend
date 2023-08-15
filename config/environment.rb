6fa96b7d196d5c10e9d3944848aad02cf9ef7ce1
Ezequiel Salas <salaseze@gmail.com>
2023-08-15 14:56:47 -0300

    react host constant

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative "application"
 
+REACT_HOST = ENV['REACT_HOST'] || 'http://localhost:3001'
+
 # Initialize the Rails application.
 Rails.application.initialize!
