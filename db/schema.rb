14275570513ee04259f6cee0261bc23c571e5a56
Ezequiel Salas <salaseze@gmail.com>
2023-08-17 16:06:31 -0300

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -30,15 +30,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_19_205435) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.datetime "blocked_until"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "full_name"
+    t.text "biography"
+    t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
