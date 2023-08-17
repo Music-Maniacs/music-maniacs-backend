a7c2cbd5d7ba3d67b19ae8e6ec06219c6fdbcce4
Ezequiel Salas <salaseze@gmail.com>
2023-08-17 16:05:05 -0300

    unique inex to username

--- db/migrate/20230617173601_devise_create_users.rb
+++ db/migrate/20230617173601_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users, id: :uuid do |t|
       ## Database authenticatable
-      t.string :email,              null: false
+      t.string :email,              null: false, unique: true
       t.string :username,           null: false, unique: true
       t.string :encrypted_password, null: false
 
@@ -44,6 +44,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
     end
 
     add_index :users, :email,                unique: true
+    add_index :users, :username,             unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
