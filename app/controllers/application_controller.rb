5524e66343d3131aea7a481c7ca65847e3bac34e
Ezequiel Salas <salaseze@gmail.com>
2023-08-17 16:06:15 -0300

    rescue_from ActiveRecord::RecordNotFound in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   protected
 
@@ -7,5 +8,11 @@ class ApplicationController < ActionController::API
     added_attrs = %i[username email password password_confirmation full_name]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: %i[login password]
+  end
+
+  private
+
+  def record_not_found
+    render json: { error: :record_not_found }, status: :not_found
   end
 end
