bee7b2e3c36fda3c0e006c8dcdc3f09827f27dbd
Ezequiel Salas <salaseze@gmail.com>
2023-08-17 16:05:34 -0300

    admin users controller and routes

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,50 @@
+class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user.as_json(methods: :state)
+  end
+
+  def create
+    @user = User.new(user_params_create)
+
+    if @user.save
+      render json: @user, status: :ok
+    else
+      render json: @user.errors.details, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params_update)
+      render json: @user, status: :ok
+    else
+      render json: @user.errors.details, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      head :no_content, status: :ok
+    else
+      render json: @user.errors.details, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params_create
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :full_name, :biography)
+  end
+
+  def user_params_update
+    params.require(:user).permit(:username, :email, :full_name, :biography)
+  end
+end
