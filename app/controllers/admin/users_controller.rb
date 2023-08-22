45f80922aae6dd48aac6e119e34b723e5f591853
Ezequiel Salas <salaseze@gmail.com>
2023-08-22 18:35:02 -0300

    al pedo variables de instancia

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,42 +1,42 @@
 class Admin::UsersController < ApplicationController
   def index
-    @q = users_scope.ransack(params[:q])
-    @users = @q.result(distinct: true).page(params[:page]).per(params[:per_page])
+    q = users_scope.ransack(params[:q])
+    users = q.result(distinct: true).page(params[:page]).per(params[:per_page])
 
-    render json: { data: @users.as_json(methods: :state), pagination: pagination_info(@users) }
+    render json: { data: users.as_json(methods: :state), pagination: pagination_info(users) }
   end
 
   def show
-    @user = users_scope.find(params[:id])
-    render json: @user.as_json(methods: :state, include: :links)
+    user = users_scope.find(params[:id])
+    render json: user.as_json(methods: :state, include: :links)
   end
 
   def create
-    @user = users_scope.new(user_params_create)
+    user = users_scope.new(user_params_create)
 
-    if @user.save
-      render json: @user.as_json(methods: :state), status: :ok
+    if user.save
+      render json: user.as_json(methods: :state), status: :ok
     else
-      render json: @user.errors.details, status: :unprocessable_entity
+      render json: user.errors.details, status: :unprocessable_entity
     end
   end
 
   def update
-    @user = users_scope.find(params[:id])
+    user = users_scope.find(params[:id])
 
-    if @user.update(user_params_update)
-      render json: @user.as_json(include: :links, methods: :state), status: :ok
+    if user.update(user_params_update)
+      render json: user.as_json(include: :links, methods: :state), status: :ok
     else
-      render json: @user.errors.details, status: :unprocessable_entity
+      render json: user.errors.details, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @user = User.find(params[:id])
-    if @user.destroy
+    user = User.find(params[:id])
+    if user.destroy
       head :no_content, status: :ok
     else
-      render json: @user.errors.details, status: :unprocessable_entity
+      render json: user.errors.details, status: :unprocessable_entity
     end
   end
 
