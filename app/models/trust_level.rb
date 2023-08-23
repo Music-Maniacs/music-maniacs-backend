3bf9cb7af07f9419969c27ed410c286209a84b3a
Ezequiel Salas <salaseze@gmail.com>
2023-08-23 19:21:53 -0300

    trust_level.rb

--- app/models/trust_level.rb
+++ app/models/trust_level.rb
@@ -1,2 +1,15 @@
 class TrustLevel < Role
+  self.ignored_columns = %w[]
+  ##############################################################################
+  # VALIDATIONS
+  ##############################################################################
+  validates :order, :days_visited, :viewed_events, :likes_received, :likes_given, :comments_count, presence: true
+  validates :order, uniqueness: true, numericality: { only_integer: true, greater_than: 0 }
+
+  ##############################################################################
+  # CLASS METHODS
+  ##############################################################################
+  def self.default_trust_level
+    find_by(order: 1)
+  end
 end
