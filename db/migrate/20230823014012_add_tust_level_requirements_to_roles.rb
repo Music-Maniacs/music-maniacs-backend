f0d7650841ac075d5c9a641041c8a2270935ae6c
Ezequiel Salas <salaseze@gmail.com>
2023-08-23 19:22:23 -0300

    migra

--- db/migrate/20230823014012_add_tust_level_requirements_to_roles.rb
+++ db/migrate/20230823014012_add_tust_level_requirements_to_roles.rb
@@ -1,6 +1,7 @@
 class AddTustLevelRequirementsToRoles < ActiveRecord::Migration[7.0]
   def change
-    update_table :roles do |t|
+    change_table(:roles) do |t|
+      t.string :type
       t.integer :order
       t.integer :days_visited
       t.integer :viewed_events
