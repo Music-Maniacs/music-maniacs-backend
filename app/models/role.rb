f4f537f9e3f0e2bd6dab206ddc0fcf5d7ec0069a
Ezequiel Salas <salaseze@gmail.com>
2023-08-24 16:54:48 -0300

    ransackable_attributes for roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -11,4 +11,11 @@ class Role < ApplicationRecord
   # VALIDATIONS
   ##############################################################################
   validates :name, presence: true, uniqueness: true
+
+  ##############################################################################
+  # CLASS METHODS
+  ##############################################################################
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[name]
+  end
 end
