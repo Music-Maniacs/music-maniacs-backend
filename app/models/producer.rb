a5e476a9bd43b457f8dca477eb7a1e8496f3c28a
Lucas Miranda <93276843+LucasMirandaD@users.noreply.github.com>
2023-09-03 19:44:06 -0300

    ABMVB Productoras (#9)
    * Productoras
    
    * Migraciones
    
    * Correccion de controller y documentacion
    
    ---------
    
    Co-authored-by: Ezequiel Salas <salaseze@gmail.com>

---
+++ app/models/producer.rb
@@ -0,0 +1,25 @@
+class Producer < ApplicationRecord
+  has_paper_trail
+  ##############################################################################
+  # ASSOCIATIONS
+  ##############################################################################
+  has_many :genreable_associations, as: :genreable
+  has_many :genres, through: :genreable_associations
+
+  has_one :image, as: :imageable, dependent: :destroy
+
+  has_many :links, as: :linkeable
+  accepts_nested_attributes_for :links, allow_destroy: true
+  ##############################################################################
+  # VALIDATIONS
+  ##############################################################################
+  validates :name, uniqueness: true
+  validates :name, :nationality, :description, presence: true
+
+  ##############################################################################
+  # CLASS METHODS
+  ##############################################################################
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[name]
+  end
+end
