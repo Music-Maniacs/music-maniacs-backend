5e6c491934e92d5797f381df1f4dd1dadf22d845
Ezequiel Salas <salaseze@gmail.com>
2023-09-12 16:54:36 -0300

    comment set_default_request_format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::API
-  before_action :set_default_request_format
+  # before_action :set_default_request_format
   before_action :set_paper_trail_whodunnit
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
-  def set_default_request_format
-    request.format = :json unless params[:format]
-  end
+  # def set_default_request_format
+  #   request.format = :json unless params[:format]
+  # end
 
   protected
 
