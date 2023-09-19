918843714bf0d8547eb748f224f4d520d3c3d231
Ezequiel Salas <salaseze@gmail.com>
2023-09-19 19:44:26 -0300

    anonymous key for reviews and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   COMMENT_TO_JSON = { only: %i[id body created_at],
-                      include: { user: { only: %i[id full_name] } } }.freeze
+                      include: { user: { only: %i[id full_name] } },
+                      methods: %i[anonymous] }.freeze
 
   before_action :authenticate_user!, except: %i[index]
 
