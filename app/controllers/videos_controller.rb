ab2bb84cb1491b026000f911355cd98939b018d5
Ezequiel Salas <salaseze@gmail.com>
2023-09-20 19:12:27 -0300

    enhacements

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,23 +1,20 @@
 class VideosController < ApplicationController
   before_action :authenticate_user!, only: %i[create destroy]
+
   VIDEO_TO_SHOW = { only: %i[id created_at recorded_at],
                     methods: %i[full_url],
-                    include:
-                      { user:
-                        { only: %i[id username] } } }.freeze
-  VIDEO_TO_JSON = { methods: %i[full_url] }.freeze
+                    include: { user: { only: %i[id username] } } }.freeze
 
   def create
     event = Event.find(params[:id])
-    video = Video.new
+    video = event.videos.build
 
     video.file.attach(params[:video]) # Asigna el archivo cargado a la instancia de Video
     video.recorded_at = params[:recorded_at] if params[:recorded_at].present?
     video.user = current_user
-    event.videos << video
 
     if video.save
-      render json: video.as_json(VIDEO_TO_JSON), status: :ok
+      head :no_content, status: :ok
     else
       render json: { errors: video.errors.details }, status: :unprocessable_entity
     end
@@ -26,7 +23,7 @@ class VideosController < ApplicationController
   def destroy
     video = Video.find(params[:video_id])
 
-    if video.destroy && video.event.videos.delete(video)
+    if video.destroy
       head :no_content, status: :ok
     else
       render json: { errors: video.errors.details }, status: :unprocessable_entity
@@ -34,14 +31,10 @@ class VideosController < ApplicationController
   end
 
   def show
-    event = Event.find(params[:id])
+    videos = Event.find(params[:id]).ransack(params[:q])
+    videos.sorts = 'recorded_at asc' if params[:sort].blank?
 
-    if event.videos.present?
-      videos = event.videos
-      render json: videos.as_json(VIDEO_TO_SHOW), status: :ok
-    else
-      render head :no_content, status: :ok
-    end
+    render json: videos.result.as_json(VIDEO_TO_SHOW), status: :ok
   end
 
   private
