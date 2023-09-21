9b1daa79c128003bd552f3e74a037e408cf5363e
Ezequiel Salas <salaseze@gmail.com>
2023-09-20 22:43:15 -0300

    send event update notifications to followers (#27)
    * wip
    
    * ahora si
    
    * mejor asi
    
    * moved cte up

--- app/mailers/events_notification_mailer.rb
+++ app/mailers/events_notification_mailer.rb
@@ -4,4 +4,11 @@ class EventsNotificationMailer < ApplicationMailer
     @event = params[:event]
     mail(to: @user.email, subject: 'nuevo evento de un perfil que sigues!')
   end
+
+  def event_updates_to_followers
+    @user = params[:user]
+    @event = params[:event]
+    @changes = params[:changes]
+    mail(to: @user.email, subject: "cambios en el evento: #{@event.name}")
+  end
 end
