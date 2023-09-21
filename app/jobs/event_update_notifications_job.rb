f59aa4fa5a724bbc47e6f8292d4a7931405de9c5
Ezequiel Salas <salaseze@gmail.com>
2023-09-20 22:39:22 -0300

    mejor asi

--- app/jobs/event_update_notifications_job.rb
+++ app/jobs/event_update_notifications_job.rb
@@ -3,9 +3,7 @@ class EventUpdateNotificationsJob < ApplicationJob
     event = Event.find_by(id: event_id)
     return unless event
 
-    users_to_notify = event.followers.pluck(:id)
-
-    User.where(id: users_to_notify).find_each do |user|
+    event.followers.find_each do |user|
       EventsNotificationMailer.with(user:, event:, changes:).event_updates_to_followers.deliver_now
     end
   end
