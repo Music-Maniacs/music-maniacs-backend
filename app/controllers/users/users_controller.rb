0971c1eeea71243d3311eb456cba8fb9336d67df
LucasMirandaD <lucasdavidmiranda8@gmail.com>
2023-09-22 19:52:03 -0300

    Gestionar Seguimientos

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -82,11 +82,46 @@ class Users::UsersController < ApplicationController
     end
   end
 
-  private
+  def show_followed
+    # Busca en params [:q] si es busqueda por nombre, tipo o incluye params[:q]
+    params_name = params.dig(:q, :followable_name_cont).gsub('"', '') if params.dig(:q, :followable_name_cont).present?
+    params_type = params.dig(:q, :followable_type_cont) if params.dig(:q, :followable_type_cont).present?
 
-  # def validate_image_count
-  #   current_user.validate_image_count
-  # end
+    if params_name.present? # Busqueda por nombre
+      followable_name = params_name # Recupera de params
+
+      # Busca en las entidades que coincidan con :followable_name_cont
+      followed_entities = current_user.follows.includes(:followable).where(followable_type: %w[Venue Producer Artist Event])
+
+      # Filtra las entidades basándonos en el atributo followable_name
+      followed_entities = followed_entities.select do |follow|
+        followable = follow.followable
+        followable.present? && followable.name.include?(followable_name)
+      end
+    elsif params_type.present? # Busqueda por tipo
+      followed_entities = current_user.follows.includes(:followable)
+                                      .where(followable_type: params_type)
+    else # Todas las entidades si no viene followable_name
+      followed_entities = current_user.follows.includes(:followable)
+    end
+    # followed_entities sea un ActiveRecord::Relation
+    followed_entities = Follow.where(id: followed_entities.map(&:id))
+
+    # Pagina los resultados
+    followed_entities = followed_entities.page(params[:page]).per(params[:per_page])
+
+    # Mapea los datos
+    follow_data = followed_entities.map do |follow|
+      {
+        followable_id: follow.followable_id,
+        followable_type: follow.followable_type,
+        name: follow.followable_name
+      }
+    end
+    render json: { data: follow_data, pagination: pagination_info(followed_entities) }, status: :ok
+  end
+
+  private
 
   def user_params_update
     JSON.parse(params.require(:user)).deep_symbolize_keys.slice(:username,
