bde88538dac8e89d414855ffcbe1ef1a7820905d
Lucas Miranda <93276843+LucasMirandaD@users.noreply.github.com>
2023-09-22 16:09:35 -0300

    Update app/controllers/artists_controller.rb
    Co-authored-by: Ezequiel Salas <salaseze@gmail.com>

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -12,11 +12,11 @@ class ArtistsController < ApplicationController
     artist = Artist.find(params[:id])
     artist_json = artist.as_json(ARTIST_TO_JSON)
 
-    if current_user.present?
-      artist_json['followed_by_current_user'] = current_user.follows?(artist)
-    else
-      artist_json['followed_by_current_user'] = false
-    end
+    artist_json['followed_by_current_user'] = if current_user.present?
+                                                current_user.follows?(artist)
+                                              else
+                                                false
+                                              end
     render json: artist_json, status: :ok
   end
                   
