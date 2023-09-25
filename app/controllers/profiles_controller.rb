825c2ebabf9324867ad4ce988b7928fde4c0cedb
Ezequiel Salas <salaseze@gmail.com>
2023-09-25 20:05:44 -0300

    wippp

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,6 +4,23 @@ class ProfilesController < ApplicationController
     producers = Producer.ransack(params[:q]).result(distinct: true).page(params[:page]).per(params[:per_page])
     venues = Venue.ransack(params[:q]).result(distinct: true).page(params[:page]).per(params[:per_page])
 
-    render json: 
+    render json: { artists: { data: artists, pagination: pagination_info(artists) },
+                   producers: { data: producers, pagination: pagination_info(producers) },
+                   venues: { data: venues, pagination: pagination_info(venues) } }
+  end
+
+  def search_artists
+    artists = Artist.ransack(params[:q]).result(distinct: true).page(params[:page]).per(params[:per_page])
+    render json: { data: artists, pagination: pagination_info(artists) }
+  end
+
+  def search_producers
+    producers = Producer.ransack(params[:q]).result(distinct: true).page(params[:page]).per(params[:per_page])
+    render json: { data: producers, pagination: pagination_info(producers) }
+  end
+
+  def search_venues
+    venues = Venue.ransack(params[:q]).result(distinct: true).page(params[:page]).per(params[:per_page])
+    render json: { data: venues, pagination: pagination_info(venues) }
   end
 end
