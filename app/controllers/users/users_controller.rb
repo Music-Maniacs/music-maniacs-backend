2e1de9dc6394cc6f97e2cc0244a32b7e6a79ee4e
LucasMirandaD <lucasdavidmiranda8@gmail.com>
2023-09-27 11:33:12 -0300

    update users_controller

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -6,16 +6,10 @@ class Users::UsersController < ApplicationController
   end
 
   def change_password
-    # Para recorrer seguramente el anidamiento
-    if params.dig(:user, :password).present? &&
-       params.dig(:user, :password_confirmation).present? &&
-       params.dig(:user, :password_confirmation) == params.dig(:user, :password)
-
-      if current_user.update(user_params_change_password)
-        render head: :no_content, status: :ok
-      else
-        render json: { errors: user.errors.details }, status: :unprocessable_entity
-      end
+    if current_user.update(user_params_change_password)
+      render head: :no_content, status: :ok
+    else
+      render json: { errors: current_user.errors.details }, status: :unprocessable_entity
     end
   end
 
