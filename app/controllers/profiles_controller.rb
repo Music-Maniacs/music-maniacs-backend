4c217d08cefd44f5cc673dba70b8a1d30e0edb73
Ezequiel Salas <salaseze@gmail.com>
2023-09-28 20:02:37 -0300

    added last_reviews to user show

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,9 @@ class ProfilesController < ApplicationController
 
   USER_TO_JSON = { only: %i[username full_name biography],
                    include: { links: { only: %i[id url title] },
+                              last_reviews: { only: %i[id rating description created_at reviewable_type],
+                                              include: { user: { only: %i[id full_name] } },
+                                              methods: :anonymous },
                               # images: { only: %i[id], methods: %i[full_url] },
                               # user_stat: { only: %i[id days_visited viewed_events likes_given likes_received comments_count last_session penalty_score] }
                               role: { only: %i[id name] } } }.freeze
