0b3c1a3ce548aac355808506032b2bccd42aedda
LucasMirandaD <lucasdavidmiranda8@gmail.com>
2023-10-03 10:37:19 -0300

    seguidos sin index

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,6 @@ Rails.application.routes.draw do
     get :show_followed_producers
     get :show_followed_events
     get :show_followed_venues
-    get :index_followed
     get '/:id', action: :show
   end
 
