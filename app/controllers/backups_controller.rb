2e36d5439b6bb6fa54b31db4041716f50d4fb261
LucasMirandaD <lucasdavidmiranda8@gmail.com>
2023-10-06 14:34:18 -0300

    delete backup

--- app/controllers/backups_controller.rb
+++ app/controllers/backups_controller.rb
@@ -1,5 +1,8 @@
 class BackupsController < ApplicationController
   BACKUP_DIR = File.expand_path('~/backups/mm_backup').freeze
+  CONTAINER_NAME = 'docker_services-db-1'.freeze
+  USER_DB = 'docker'.freeze
+  DB = 'music_maniacs_backend_development'.freeze
 
   def index
     backup_files = Dir.glob("#{BACKUP_DIR}/*").map { |folder| File.basename(folder) }
@@ -12,25 +15,41 @@ class BackupsController < ApplicationController
 
   def restore_backup
     selected_backup = params[:id]
-    container_name = 'docker_services-db-1'
-    db = 'music_maniacs_backend_development'
-    user = 'docker'
     backup_file_path = "#{BACKUP_DIR}/#{selected_backup}/mm_backup/databases/PostgreSQL.sql"
-    tar_file_path = "#{BACKUP_DIR}/#{selected_backup}.tar" # Cambio aquí
+    tar_file_path = "#{BACKUP_DIR}/#{selected_backup}/mm_backup.tar" # Cambio aquí
 
     # Limpiar la base de datos antes de restaurar el backup
-    clean_database_command = "sudo docker exec -i #{container_name} psql -U #{user} -d #{db} -c 'DROP SCHEMA public CASCADE; CREATE SCHEMA public;'"
+    clean_database
 
-    # Restaurar la base de datos desde el archivo PostgreSQL.sql
-    restore_command = "sudo docker exec -i #{container_name} psql -U #{user} -d #{db} < #{backup_file_path}"
+    # Ruta para restaurar la base de datos desde el archivo PostgreSQL.sql
+    restore_command = "sudo docker exec -i #{CONTAINER_NAME} psql -U #{USER_DB} -d #{DB} < #{backup_file_path}"
 
-    # Ejecutar los comandos para limpiar y restaurar la base de datos
-    system(clean_database_command) # limpia la db
+    # Descomprime y restaura
+
     system("tar -xf #{tar_file_path}") # descomprime
     system(restore_command) # restaura la db
 
-    render json: { message: 'Backup restaurado exitosamente' }, status: :ok
+    head :no_content
   rescue StandardError => e
-    render json: { error: "Error al restaurar el backup: #{e.message}" }, status: :unprocessable_entity
+    render json: { error: "Error al restaurar el backup: #{e.message}" }, status: :not_found
+  end
+
+  def destroy
+    selected_backup = params[:id]
+    backup_file_path_delete = "#{BACKUP_DIR}/#{selected_backup}"
+    # Existe el directorio?
+    if Dir.exist?(backup_file_path_delete)
+      FileUtils.rm_rf(backup_file_path_delete) # Borra el directorio y su contenido
+      head :no_content
+    else
+      render json: { error: 'El directorio de backup no existe' }, status: :not_found
+    end
+  end
+
+  private
+
+  def clean_database
+    clean_database_command = "sudo docker exec -i #{CONTAINER_NAME} psql -U #{USER_DB} -d #{DB} -c 'DROP SCHEMA public CASCADE; CREATE SCHEMA public;'"
+    system(clean_database_command) # limpia la db
   end
 end
