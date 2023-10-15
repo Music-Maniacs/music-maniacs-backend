ef20eaca5bb2fa5f888bee8ef27daa5cb766d425
LucasMirandaD <lucasdavidmiranda8@gmail.com>
2023-10-15 12:46:21 -0300

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
   constraints(id: /[^\/]+/) do
     resources :backups, only: %i[index destroy create] do
       member do
-        post :restore
+        post :restore_backup
       end
     end
   end
