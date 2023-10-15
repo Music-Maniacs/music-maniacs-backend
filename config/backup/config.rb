a24afbf48da7706613c0a8229766201f73d61c15
Lucas Miranda <93276843+LucasMirandaD@users.noreply.github.com>
2023-10-15 19:48:03 -0300

    Backup (#42)
    * wip
    
    * whenever
    
    * corecciones menores
    
    * correciones menores, sigue dando error
    
    * endpoints, doc - wip
    
    * correcciones menores
    
    * Restore backup
    
    * delete backup
    
    * rutas
    
    * doc
    
    * fix tar error
    
    * actualizacion index
    
    * Rubocop, comentarios
    
    * cambios rutas y status
    
    * data
    
    * create_backup
    
    * shhhh
    
    * pagination
    
    * routes.rb
    
    * create
    
    * Update README.md
    
    * update README.md
    
    * tomi --> pipipipi

---
+++ config/backup/config.rb
@@ -0,0 +1,50 @@
+# encoding: utf-8
+
+##
+# Backup
+# Generated Main Config Template
+#
+# For more information:
+#
+# View the Git repository at https://github.com/meskyanichi/backup
+# View the Wiki/Documentation at https://github.com/meskyanichi/backup/wiki
+# View the issue log at https://github.com/meskyanichi/backup/issues
+
+##
+# Utilities
+#
+# If you need to use a utility other than the one Backup detects,
+# or a utility can not be found in your $PATH.
+#
+Backup::Utilities.configure do
+  pg_dump '/usr/bin/pg_dump' # Debe apuntar a la ruta dentro del contenedor
+end
+
+##
+# Logging
+#
+# Logging options may be set on the command line, but certain settings
+# may only be configured here.
+#
+Backup::Logger.configure do
+  # Logfile options:
+  console.quiet     = true # quitar el commentario para no mostrar informacion en consola
+  logfile.enabled   = true
+  logfile.log_path  = 'log'
+  logfile.max_bytes = 500_000
+end
+#
+# Command line options will override those set here.
+# For example, the following would override the example settings above
+# to disable syslog and enable console output.
+#   backup perform --trigger my_backup --no-syslog --no-quiet
+
+# * * * * * * * * * * * * * * * * * * * *
+#        Do Not Edit Below Here.
+# All Configuration Should Be Made Above.
+
+##
+# Load all models from the models directory.
+Dir[File.join(File.dirname(Config.config_file), "models", "*.rb")].each do |model|
+  instance_eval(File.read(model))
+end
