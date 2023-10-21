203a36684277c5a0023059b4c67eb1353928a59e
Ezequiel Salas <salaseze@gmail.com>
2023-10-21 19:41:00 -0300

    ups

--- app/models/report.rb
+++ app/models/report.rb
@@ -108,8 +108,8 @@ class Report < ApplicationRecord
   def merge_profile(duplicated:, original:)
     profile_type = reportble.class.to_s.downcase # artist, venue, producer
     duplicated.events.update_all("#{profile_type}_id" => original.id)
-    duplicated.reviews.update_all(reviewable_id: original)
-    duplicated.follows.update_all(followable_id: original)
+    duplicated.reviews.update_all(reviewable_id: original.id)
+    duplicated.follows.update_all(followable_id: original.id)
     duplicated.destroy!
   end
 
