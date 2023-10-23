febb0bac81cff78d0df6f6af8bdd1d0021ddad1e
Ezequiel Salas <salaseze@gmail.com>
2023-10-23 20:56:58 -0300

    eso

--- app/models/concerns/versionable.rb
+++ app/models/concerns/versionable.rb
@@ -2,7 +2,7 @@ module Versionable
   extend ActiveSupport::Concern
 
   included do
-    has_paper_trail class_name: 'Version', ignore: try(:ignored_version_attrs)
+    has_paper_trail class_name: 'Version', ignore: try(:ignored_version_attrs) || []
   end
 
   def author_id_by_versions
