ad5715aa9fc905ba5d52fc9a44c60b3d67cf1bdd
Lucas Miranda <93276843+LucasMirandaD@users.noreply.github.com>
2023-10-24 12:09:27 -0300

    backup_multimedia (#46)
    * backup_multimedia wip
    
    * create backup_multimedia
    
    * index MB
    
    * Restore Multimedia
    
    * lib
    
    * legilibilidad
    
    * validacion param multimedia
    
    * status
    
    * multimedia param, lib
    
    * Delete app/controllers/metrics_controller.rb

--- app/controllers/backups_controller.rb
+++ app/controllers/backups_controller.rb
@@ -1,6 +1,7 @@
 class BackupsController < ApplicationController
   before_action :authenticate_user!
   BACKUP_DIR = File.expand_path('~/backups/mm_backup').freeze
+  MULTIMEDIA_DIR = File.expand_path('./storage').freeze
   CONTAINER_NAME = 'docker_services-db-1'.freeze
   USER_DB = 'docker'.freeze
   DB = 'music_maniacs_backend_development'.freeze
@@ -11,8 +12,9 @@ class BackupsController < ApplicationController
     paginated_backups = Kaminari.paginate_array(dir).page(params[:page]).per(params[:per_page])
 
     backup_data = paginated_backups.map do |folder|
-      folder_size_bytes = Dir.glob("#{folder}/*/").select { |f| File.file?(f) }.map { |f| File.size(f) }.sum
-      folder_size_megabytes = folder_size_bytes / 1_048_576.0  # Convertir bytes a megabytes
+      folder_size_bytes = Dir.glob("#{folder}/**/*").select { |f| File.file?(f) }.map { |f| File.size(f) }.sum
+      folder_size_megabytes = folder_size_bytes / 1_048_576.0 # Convertir bytes a megabytes
+
       {
         name: File.basename(folder),
         size_megabytes: folder_size_megabytes.round(2), # Tamaño MB
@@ -26,17 +28,22 @@ class BackupsController < ApplicationController
   def restore_backup
     selected_backup = params[:id]
     backup_file_path = "#{BACKUP_DIR}/#{selected_backup}/mm_backup/databases/PostgreSQL.sql"
-    tar_file_path = "#{BACKUP_DIR}/#{selected_backup}" # Cambio aquí
+    tar_file_path = "#{BACKUP_DIR}/#{selected_backup}"
+    path_multimedia = "#{BACKUP_DIR}/#{selected_backup}/multimedia*"
 
     # Limpiar la base de datos antes de restaurar el backup
     clean_database
 
     # Ruta para restaurar la base de datos desde el archivo PostgreSQL.sql
-    restore_command = "sudo docker exec -i #{CONTAINER_NAME} psql -U #{USER_DB} -d #{DB} < #{backup_file_path}"
+    restore_command = "docker exec -i #{CONTAINER_NAME} psql -U #{USER_DB} -d #{DB} < #{backup_file_path}"
 
     # Descomprime y restaura
     system("cd #{tar_file_path} && tar -xf mm_backup.tar") # descomprime
     system(restore_command) # restaura la db
+
+    matching_files = Dir.glob(path_multimedia)
+
+    multimedia_restore(matching_files)
 
     head :no_content, status: :ok
   rescue StandardError => e
@@ -49,7 +56,7 @@ class BackupsController < ApplicationController
     # Existe el directorio?
     if Dir.exist?(backup_file_path_delete)
       FileUtils.rm_rf(backup_file_path_delete) # Borra el directorio y su contenido
-      head :no_content
+      head :no_content, status: :ok
     else
       render json: { error: 'El directorio de backup no existe' }, status: :not_found
     end
@@ -60,8 +67,12 @@ class BackupsController < ApplicationController
     dir = Dir.glob("#{BACKUP_DIR}/*")
     latest_folder = dir.max_by { |folder| File.ctime(folder) }
 
+    multimedia_create(latest_folder) # backup multimedia
+
+    # Despues de todas las operaciones calculo el tamaño
+
     folder_size_bytes = Dir.glob("#{latest_folder}/**/*").select { |f| File.file?(f) }.map { |f| File.size(f) }.sum
-    folder_size_megabytes = folder_size_bytes / 1_048_576.0  # Convertir bytes a megabytes
+    folder_size_megabytes = folder_size_bytes / 1_048_576.0 # Convertir bytes a megabytes
 
     latest_backup_info = {
       name: File.basename(latest_folder),
@@ -77,7 +88,7 @@ class BackupsController < ApplicationController
   private
 
   def clean_database
-    clean_database_command = "sudo docker exec -i #{CONTAINER_NAME} psql -U #{USER_DB} -d #{DB} -c 'DROP SCHEMA public CASCADE; CREATE SCHEMA public;'"
+    clean_database_command = "docker exec -i #{CONTAINER_NAME} psql -U #{USER_DB} -d #{DB} -c 'DROP SCHEMA public CASCADE; CREATE SCHEMA public;'"
     system(clean_database_command) # limpia la db
   end
 
@@ -89,5 +100,43 @@ class BackupsController < ApplicationController
 
     # Convertir la cadena de fecha a un objeto de fecha
     DateTime.strptime(date_str, '%Y.%m.%d.%H.%M.%S')
+  end
+
+  def multimedia_restore(path_multimedia)
+    temp_extract_dir = "#{MULTIMEDIA_DIR}/temp_extract" # Carpeta temporal
+
+    FileUtils.mkdir_p(temp_extract_dir) # Crear carpeta temporal si no existe
+
+    # Copiar el archivo comprimido a la carpeta temporal
+    FileUtils.cp_r(path_multimedia, temp_extract_dir)
+
+    # Descomprimir el archivo TAR en la carpeta temporal
+    system("cd #{temp_extract_dir} && tar -xf multimedia*.tar")
+
+    # Copiar los archivos extraídos al directorio MULTIMEDIA_DIR
+    FileUtils.cp_r("#{temp_extract_dir}/.", MULTIMEDIA_DIR)
+
+    # Eliminar el archivo TAR
+    tar_file = Dir.glob("#{MULTIMEDIA_DIR}/multimedia*.tar")
+    FileUtils.rm(tar_file)
+
+    # Eliminar la carpeta temporal
+    FileUtils.rm_r(temp_extract_dir)
+  end
+
+  def multimedia_create(latest_folder)
+    storage_directory = MULTIMEDIA_DIR
+    backup_directory = latest_folder
+
+    # Nombre del archivo de respaldo comprimido
+    backup_filename = "multimedia-#{Time.now.strftime('%Y%m%d%H%M%S')}.tar"
+
+    # Comprimir el directorio de almacenamiento en un archivo TAR
+    Dir.chdir(storage_directory) do
+      system("tar -cvf #{backup_filename} .")
+    end
+
+    # Mover el archivo comprimido a la carpeta de backups
+    FileUtils.mv("#{storage_directory}/#{backup_filename}", backup_directory)
   end
 end
