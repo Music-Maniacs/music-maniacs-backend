43362d6b82ef49d8704df2f5dbeb81c68d7c1939
LucasMirandaD <lucasdavidmiranda8@gmail.com>
2023-11-05 20:21:56 -0300

    correccion mailer

--- app/jobs/event_update_notifications_job.rb
+++ app/jobs/event_update_notifications_job.rb
@@ -7,14 +7,4 @@ class EventUpdateNotificationsJob < ApplicationJob
       EventsNotificationMailer.with(user:, event:, changes:).event_updates_to_followers.deliver_now
     end
   end
-
-  class EventDestroyNotificationsJob < ApplicationJob
-    def perform(event_id)
-      event = Event.find_by(id: event_id)
-      return unless event
-  
-      event.followers.find_each do |user|
-        EventsNotificationMailer.with(user: user, event: event, deleted: true).event_destroy_to_followers.deliver_now
-      end
-    end
 end
