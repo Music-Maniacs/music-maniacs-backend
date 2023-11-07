da288db8c8a846886a03e5c49a6799c878fb71d2
LucasMirandaD <lucasdavidmiranda8@gmail.com>
2023-11-07 11:26:28 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_05_002553) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_05_205858) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -300,7 +300,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_05_002553) do
     t.index ["version_id"], name: "index_version_associations_on_version_id"
   end
 
-  create_table "versions", force: :cascade do |t|
+  create_table "versions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "item_type", null: false
     t.string "item_id", null: false
     t.string "event", null: false
